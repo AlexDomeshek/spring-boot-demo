2f52c15cd6d87eb8488164c3ae88ce7e26d1c8d9
Alex Domeshek <alex@labsuit.com>
2018-01-18 13:37:54 +0200

    Added example of Spring Configuration properties

---
+++ src/main/java/org/demo/springbootdemo/autoscan/Band.java
@@ -0,0 +1,25 @@
+package org.demo.springbootdemo.autoscan;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@ConfigurationProperties(prefix = "beatles")
+@Component
+public class Band {
+    private List<String> members = new ArrayList<String>();
+
+    public List<String> getMembers() {
+        return members;
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Band members: " + members);
+    }
+}
+
