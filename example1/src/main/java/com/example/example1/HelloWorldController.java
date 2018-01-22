26c1cffe95590d90758d53862334ea2cb9749f92
Alex Domeshek <alex@labsuit.com>
2018-01-22 12:14:55 +0200

    Splitting into multi-module maven app: 1st module: example1

---
+++ example1/src/main/java/com/example/example1/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.example.example1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by alex on 1/18/18.
+ */
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/sayHello")
+    String sayHello(@RequestParam("name") String name){
+        return "Hello, "+name;
+    }
+}
